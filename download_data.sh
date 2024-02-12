7e00e617e968a2f42af8c27e0249ec18318b1286
Jessica Taylor <rstudio@Jessica-Laptop>
2024-02-12 12:28:22 +0000

    Shell scripts for downloading ChIP-seq data and executing the peakcaller.

---
+++ download_data.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+function get_data { fasterq-dump $1 -O $1; }
+
+echo "Provide sample datasets"
+read -a sample_data
+input_sample_data=$(echo "${sample_data[*]}" | tr ' ' ,)
+
+echo "Is a control included - yes or no?"
+read control_included
+
+if [ "$control_included" = "yes" ];
+then
+	echo "Provide control datasets";
+	read -a control_data;
+	input_control_data=$(echo "${control_data[*]}" | tr ' ' ,)
+fi
+
+for file in "${sample_data[@]}"; do get_data "$file"; cd "$file"; rm -r *.sra*; cd ..; done;
+fi
--- run_analysis.sh
+++ run_analysis.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-function get_data { fasterq-dump $1 -O $1; }
-
 echo "Is a control included - yes or no?"
 read control_included
 
@@ -25,34 +23,25 @@ if [ "$control_included" = "no" ];
 then
 	if [ "$sequencing_type" = "single" ];
 	then
-		for file in "${sample_data[@]}"; do if [ ! -f "$file" ]; then get_data "$file"; fi; done;
-		nextflow run callPeaks.nf --genome-fasta TAIR10_chr_all.fas --chip-seq-fastq "$input_sample_data" --use-rmdup;
+		nextflow run callPeaks.nf --genome-fasta Triticum_aestivum.IWGSC.dna.toplevel.fa --chip-seq-fastq "$input_sample_data" --use-rmdup;
 	elif [ "$sequencing_type" = "paired" ];
 	then
-		for file in "${sample_data[@]}"; do if [ ! -f "$file" ]; then get_data "$file"; fi; done;
-		nextflow run callPeaks.nf --genome-fasta TAIR10_chr_all.fas --chip-seq-fastq "$input_sample_data" --paired --use-rmdup;
+		nextflow run callPeaks.nf --genome-fasta Triticum_aestivum.IWGSC.dna.toplevel.fa --chip-seq-fastq "$input_sample_data" --paired --use-rmdup;
 	fi
 elif [ "$control_included" = "yes" ];
 then
 	if [ "$sequencing_type" = "single" ];
 	then
-		for file in "${sample_data[@]}" "${control_data[@]}"; do if [ ! -f "$file" ]; then get_data "$file"; fi; done;
-		nextflow run callPeaks.nf --genome-fasta TAIR10_chr_all.fas --chip-seq-fastq "$input_sample_data" --control-fastq "$input_control_data" --use-rmdup;
+		nextflow run callPeaks.nf --genome-fasta Triticum_aestivum.IWGSC.dna.toplevel.fa --chip-seq-fastq "$input_sample_data" --control-fastq "$input_control_data" --use-rmdup;
 	elif [ "$sequencing_type" = "paired" ];
 	then
-		for file in "${sample_data[@]}" "${control_data[@]}"; do if [ ! -f "$file" ]; then get_data "$file"; fi; done;
-		nextflow run callPeaks.nf --genome-fasta TAIR10_chr_all.fas --chip-seq-fastq "$input_sample_data" --control-fastq "$input_control_data" --paired --use-rmdup;
+		nextflow run callPeaks.nf --genome-fasta Triticum_aestivum.IWGSC.dna.toplevel.fa --chip-seq-fastq "$input_sample_data" --control-fastq "$input_control_data" --paired --use-rmdup;
 	fi
 fi
 
-
-rm -rf SRR*;
 cd work; rm -rf *; cd ..;
 cd output;
-rm -rf SRR*;
 mv mergedBroadPeaks.bed "${sample_data[0]}"mergedBroadPeaks.bed;
 mv mergedNarrowPeaks.bed "${sample_data[0]}"mergedNarrowPeaks.bed;
 cd ..;
 
-rclone copy ~/nextflow-peakcaller/output OneDrive:ChIP-seq-enrichment-analysis/Nextflow_backup
-#rm -r output
